14b13e676ddb5fe29d0cd377e4babf6e0ede93c1
DeZoST <zosted62400@gmail.com>
2024-07-17 15:47:04 +0200

    Section component

--- src/components/section/Section.jsx
+++ src/components/section/Section.jsx
@@ -1,10 +1,11 @@
 import PropTypes from "prop-types";
+import styles from "./Section.module.css";
 
-const Section = ({ title, children }) => {
+const Section = ({ title, className, children }) => {
   return (
-    <section>
-      <h2>{title}</h2>
-      <div className="section-content">{children}</div>
+    <section className={`section ${className} container`}>
+      <h2 className={styles.section__title}>{title}</h2>
+      <div className={styles.section__content}>{children}</div>
     </section>
   );
 };
@@ -12,6 +13,7 @@ const Section = ({ title, children }) => {
 Section.propTypes = {
   title: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  className: PropTypes.string,
 };
 
 export default Section;
